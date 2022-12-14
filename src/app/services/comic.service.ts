6858c84bbf59847092551eb69acc239a8c0bacf3
Eric Kurtz <erickurtz@live.com>
2022-12-14 09:13:13 -0600

    some formatting, import

--- src/app/services/comic.service.ts
+++ src/app/services/comic.service.ts
@@ -1,38 +1,34 @@
 import { Injectable } from '@angular/core';
 
 import { Comic } from '../models/comic';
-import { Observable, of } from 'rxjs';
-import { map, mergeMap } from 'rxjs/operators';
-import { HttpClient, HttpClientModule } from '@angular/common/http'
-import { strictEqual } from 'assert';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http'
 import { dbUrl } from '../constants';
 
 
-const currUrl = dbUrl;
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ComicService {
 
-  constructor(private http: HttpClient) { 
-  }
+    constructor(private http: HttpClient) {
+    }
 
 
-  getNavs(): Observable<string[]>{
-    return this.http.get(dbUrl+"navs").pipe(map(res =>{
-      return res["comics"] as string[];
-    }));
-  }
+    getNavs(): Observable<string[]> {
+        return this.http.get(dbUrl + "navs").pipe(map(res => {
+            return res["comics"] as string[];
+        }));
+    }
 
 
-  getComic(nav: string): Observable<Comic> { 
-    return this.http.get(dbUrl+ nav).pipe(map(res =>{ 
-     return res as Comic;
-    }))
-  }
-
- 
+    getComic(nav: string): Observable<Comic> {
+        return this.http.get(dbUrl + nav).pipe(map(res => {
+            return res as Comic;
+        }))
+    }
 
 }
 
